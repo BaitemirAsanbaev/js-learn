f40d130ef1265c03b53ffb8e9b64a42d82395594
Baitemir <ferrari170604@gmail.com>
2021-01-15 15:36:49 +0600

    the end :)

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //variables
-
+let groupName = "Beatles";
+let movieName = "Terminator";
+let myName = "Baitemir";
 //functions
-console.log('I\'m the fan of "Beatles"!');
+console.log('I\'m the fan of "' + groupName + '"!');
+console.log(`I'm the fan of "Beatles"!`);
+
+console.log(`I'm the fan of "${groupName}"!`);
+console.log(`My favourite movie is "` + movieName + `"`);
