16c8b7fae63e59533ff05164c4570adc5352d0ee
Baitemir <ferrari170604@gmail.com>
2021-01-22 14:30:30 +0600

    22.01.21

--- script.js
+++ script.js
@@ -1,77 +1,105 @@
-//variables
+// //variables
 
-//score
-let score = 0;
-//queestions
-let question1 = prompt("2 + 2");
-let answer1 = 4;
-let question2 = prompt("the highest mountain");
-let answer2 = "Everest";
-let question3 = prompt("owner of Facebook company");
-let answer3 = "Mark Zuckerberg";
-let question4 = prompt("the capital of Kyrgyzstan");
-let answer4 = "Bishkek";
-let question5 = prompt("country with the biggest population");
-let answer5 = "China";
-let question6 = prompt("Marvel Studios main producer");
-let answer6 = "Kevin Faige";
-let question7 = prompt("How many teeth peaple has");
-let answer7 = 32;
-let question8 = prompt("OS created by Linus Torvalds");
-let answer8 = "Linux";
-let questin9 = prompt("PHP, Python, Ruby, Node.js are languages for ...");
-let answer9 = "Backend";
-let question10 = prompt("What happaned in 1916");
-let answer10 = "Urkun";
-let question11 = prompt("Who is the best person ever");
-let answer11 = "Baitemir";
+// //score
+// let score = 0;
+// //queestions
+// let question1 = prompt("2 + 2");
+// let answer1 = 4;
+// let question2 = prompt("the highest mountain");
+// let answer2 = "Everest";
+// let question3 = prompt("owner of Facebook company");
+// let answer3 = "Mark Zuckerberg";
+// let question4 = prompt("the capital of Kyrgyzstan");
+// let answer4 = "Bishkek";
+// let question5 = prompt("country with the biggest population");
+// let answer5 = "China";
+// let question6 = prompt("Marvel Studios main producer");
+// let answer6 = "Kevin Faige";
+// let question7 = prompt("How many teeth peaple has");
+// let answer7 = 32;
+// let question8 = prompt("OS created by Linus Torvalds");
+// let answer8 = "Linux";
+// let questin9 = prompt("PHP, Python, Ruby, Node.js are languages for ...");
+// let answer9 = "Backend";
+// let question10 = prompt("What happaned in 1916");
+// let answer10 = "Urkun";
+// let question11 = prompt("Who is the best person ever");
+// let answer11 = "Baitemir";
 
-//functions
+// //functions
 
-//questions functions
-if(question1 == answer1){
-    score += 5;
-    console.log("1")
-};
-if(question2 == answer2){
-    score += 5;
-    console.log("2")
-};
-if(question3 == answer3){
-    score += 5;
-    console.log("3")
-};
-if(question4 == answer4){
-    score += 5;
-    console.log("4")
-};
-if(question5 == answer5){
-    score += 10;
-    console.log("5")
-};
-if(question6 == answer6){
-    score += 10;
-    console.log("6")
-};
-if(question7 == answer7){
-    score += 10;
-    console.log("7")
-};
-if(question8 == answer8){
-    score += 10;
-    console.log("8")
+// //questions functions
+// if(question1 == answer1){
+//     score += 5;
+//     console.log("1")
+// };
+// if(question2 == answer2){
+//     score += 5;
+//     console.log("2")
+// };
+// if(question3 == answer3){
+//     score += 5;
+//     console.log("3")
+// };
+// if(question4 == answer4){
+//     score += 5;
+//     console.log("4")
+// };
+// if(question5 == answer5){
+//     score += 10;
+//     console.log("5")
+// };
+// if(question6 == answer6){
+//     score += 10;
+//     console.log("6")
+// };
+// if(question7 == answer7){
+//     score += 10;
+//     console.log("7")
+// };
+// if(question8 == answer8){
+//     score += 10;
+//     console.log("8")
+// }
+// if(question9 == answer9){
+//     score += 10;
+//     console.log("9")
+// };
+// if(question10 == answer10){
+//     score += 10;
+//     console.log("10")
+// };
+// if(question11 == answer11){
+//     score +=20;
+//     console.log("11")
+// };
+// //score alert
+// alert("You have reached " + score + " points");
+
+
+
+//homework!
+
+
+
+
+
+
+
+alert("In what temoerature ice will melt?");
+
+let answer1 = prompt("unit");
+let answer2 = prompt("temperature");
+
+if(answer1 == "K" && answer2 == 273){
+    console.log("correct");
 }
-if(question9 == answer9){
-    score += 10;
-    console.log("9")
-};
-if(question10 == answer10){
-    score += 10;
-    console.log("10")
-};
-if(question11 == answer11){
-    score +=20;
-    console.log("11")
-};
-//score alert
-alert("You have reached " + score + " points");
+else if(answer1 == "C" && answer2 == 0){
+    console.log("correct");
+}
+else if(answer1 == "C" && answer2 == 32){
+    console.log("correct");
+}
+else{
+    console.log("incorrect");
+}
